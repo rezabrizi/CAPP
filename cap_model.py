d9a140bd5e3d597e498a7cf1762b567b7a2fe65c
Reza Tabrizi <rtabrizi03@gmail.com>
2024-05-14 10:02:32 -0500

    removing graph pooling, adding sigmoid

--- cap_model.py
+++ cap_model.py
@@ -5,23 +5,34 @@ import torch.nn as nn
 
 class GAT(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+        # input dim is 4
         super(GAT, self).__init__()
         # mlp for encoding
         self.mlp1 = nn.Linear(input_dim, hidden_dim)
-        self.hidden_gat_layers = nn.ModuleList([GATConv(hidden_dim, hidden_dim, heads=1, concat=False, dropout=0.2) for _ in range(num_layers)])
+        # hidden dim is 64
+        self.hidden_gat_layers = nn.ModuleList(
+            [GATConv(hidden_dim, hidden_dim, heads=1, concat=False, dropout=0.2) for _ in range(num_layers)])
         # mlp for decoding
+        # output dim is 1
         self.mlp2 = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, data):   
         x, edge_index, batch = data.x, data.edge_index, data.batch
         # encode
         x = self.mlp1(x)
+        # x = n * 64 
         for i, layer in enumerate(self.hidden_gat_layers): 
             x_in = x
-            x = F.relu(layer(x, edge_index))
+            x = layer(x, edge_index)
             x = x + x_in 
-        x = global_add_pool(x, batch)
+    
         # decode
         x = self.mlp2(x)
-        x = x.squeeze(-1)
+        # x = n * 1
+        x = torch.sigmoid(x)
+        # x = n * 1 
+        print(x.shape)
+        x = x.sum(dim=0)
+        print(x.shape)
+
         return x
